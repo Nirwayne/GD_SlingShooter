ab96b657fe49f461c62fbade12dd93a07c7f346c
Kerstin <kerstin.arnold@stud.h-da.de>
2015-07-10 21:03:43 +0200

    10.07.2015 check Out: End Screen with Score + ExitButton Fixed minor bugs

---
+++ Assets/FinalScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour {
+
+
+	Text str_score;
+	
+	void Awake(){
+		str_score = GetComponent<Text>();
+	}
+	void Update() {
+		str_score.text = ("Your Score " + gamecontroller.score);
+	}
+
+	public void exit(bool onclick) {
+		if (onclick)
+			Application.Quit();
+	}
+}
